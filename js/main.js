856ae63de63337ab1e91e72685d3905061eece65
youngmin109 <qhqo3325@gmail.com>
2025-07-01 00:11:31 +0900

    study: 3days nd images

--- js/main.js
+++ js/main.js
@@ -14,3 +14,24 @@ searchInputEl.addEventListener('blur', function() {
     searchhEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+    console.log(window.screenY);
+    if (window.scrollY > 500) {
+        // 배지 숨기기
+        // gsap.to(요소, 지속시간, 옵션)
+        gsap.to(badgeEl, .6, {
+            opacity: 0,
+            display: 'none'
+        });
+    } else {
+        // 배지 보이기
+        gsap.to(badgeEl, .6, {
+            opacity: 1,
+            display: 'block'
+        });
+    }
+}, 300));
+// _.throttle(함수, 시간(ms))
