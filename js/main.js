abccebd43c326cc38cf00be63197cfb8ab35a87e
youngmin109 <qhqo3325@gmail.com>
2025-07-07 22:42:12 +0900

    fix: 4day finish

--- js/main.js
+++ js/main.js
@@ -35,3 +35,12 @@ window.addEventListener('scroll', _.throttle(function () {
     }
 }, 300));
 // _.throttle(함수, 시간(ms))
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+    gsap.to(fadeEl, 1, {
+        delay: (index + 1) * .7,
+        opacity: 1
+    });
+});
